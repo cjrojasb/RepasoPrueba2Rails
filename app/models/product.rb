5798c15a3cae1bc0066a59881c8891c1762e78de
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-07-17 21:37:39 -0400

    2° Commit development - Add colum Premium to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
+  #belongs_to :category, optional: true -> Categoria es opcional
   belongs_to :category
+  validates :price, presence: true
+  before_save :addColumFalse
+
+  def addColumFalse
+    if self.premium.nil?
+      self.premium = false
+    end
+  end
+
 end
