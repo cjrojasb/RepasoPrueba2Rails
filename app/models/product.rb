5cc705cd5112d32d330be03eda609b6d1a037b0b
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-07-18 19:21:45 -0400

    4° commit development - add Scopes in branch Backend

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,11 @@ class Product < ApplicationRecord
   #belongs_to :category, optional: true -> Categoria es opcional
   belongs_to :category
   validates :price, presence: true
-  before_save :addColumFalse
+  before_save :check_premium
+  scope :scopePremium, -> {where(premium: true)}
+  scope :last_n, (->(n) { order(:id).limit(n) })
 
-  def addColumFalse
+  def check_premium
     if self.premium.nil?
       self.premium = false
     end
